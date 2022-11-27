9b12c149a21d321f65a0eab29e1db65b3d043c2a
root <giovannepm@gmail.com>
2022-11-27 05:47:07 -0300

    corrigindo erros

--- parateste/userpage/functionuser/recado.php
+++ parateste/userpage/functionuser/recado.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
 
-	if ((!empty($_GET['i'])) and (!empty($_GET['r'])) and (!empty($_GET['index'])) and (!empty($_GET['id']))){
+	if ((!empty($_GET['i'])) and (!empty($_GET['r'])) and (!empty($_GET['index'])) and (!empty($_GET['ap']))){
 		
 		$id=$_SESSION['id'];
 		$index=$_GET['index'];
 		$i=$_GET['i'];
 		$t=$_GET['r'];
-		$idapaga=$_GET['id'];
+		$idapaga=$_GET['ap'];
 		
-			unlink("../../other/". $id ."/recado/". $index ."/". $i ."/". $t);
+		$t=str_replace(" - ","",$t);
+		
+			unlink("../../other/". $id ."/recado/". $index ."/". $i ."/". $t .".txt");
 			unlink("../../other/". $id ."/recado/". $index ."/". $i ."/". $idapaga);
 			rmdir("../../other/". $id ."/recado/". $index ."/". $i ."/");
 			
