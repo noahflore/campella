06bb77c444d5ca2f6fdae0b703a8212408e809a2
root <giovannepm@gmail.com>
2023-03-06 20:14:51 -0300

    bbcode

--- parateste/function/bbcode.php
+++ parateste/function/bbcode.php
@@ -19,7 +19,9 @@
 				"@\[quote\=(.*?)\](.*?)\[\/quote\]@i"=>"<div style='background-color:orange;width:10px'><div class='cita'>$1</div></div><br>$2",
 				"@\[youtube=(.*?)\]\[\/youtube\]@i"=>"<iframe width='200' height='200' src='$1' title='video_user' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe>",
 				"@\[youtube=(.*?)\](.*?)\[\/youtube\]@i"=>"<fieldset><legend><h2>$2</h2></legend><iframe width='200' height='200' src='$1' title='video_user' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe></fieldset>",
-				"@\?help\?@i"=>"[b]<strong>palavra em negrito</strong>[/b]<br>[i]<i>palavra em italica</i>[/i]<br>[s]<strong>palavra sublinada</s>[/s]<br>[quote]cita um comntario de alguém[/quote]<br>[color= o valor da cor]cor da palavra[/color]<br>[youtube=url do video]um titulo por video[/youtube]<br>[img=url da image][/img]<br>"
+				"@\[link\=(.*?)\]\[\/link\]@i"=>"<a href='$1' target='_blank'>$1</a>",
+				"@\[link\=(.*?)\](.*?)\[\/link\]@i"=>"<a href='$1' target='_blank'>$2</a>",
+				"@\?help\?@i"=>"[link=url do site]titulo do site [/link]<br>[b]<strong>palavra em negrito</strong>[/b]<br>[i]<i>palavra em italica</i>[/i]<br>[s]<strong>palavra sublinada</s>[/s]<br>[quote]cita um comntario de alguém[/quote]<br>[color= o valor da cor]cor da palavra[/color]<br>[youtube=url do video]um titulo por video[/youtube]<br>[img=url da image][/img]<br>"
 		
 		
 		
