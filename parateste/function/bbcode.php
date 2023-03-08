02f4ab12f18f1feee71542967a8576d15883c808
root <giovannepm@gmail.com>
2023-03-08 13:15:50 -0300

    bbcode

--- parateste/function/bbcode.php
+++ parateste/function/bbcode.php
@@ -21,6 +21,11 @@
 				"@\[youtube=(.*?)\](.*?)\[\/youtube\]@i"=>"<fieldset><legend><h2>$2</h2></legend><iframe width='200' height='200' src='$1' title='video_user' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe></fieldset>",
 				"@\[link\=(.*?)\]\[\/link\]@i"=>"<a href='$1' target='_blank'>$1</a>",
 				"@\[link\=(.*?)\](.*?)\[\/link\]@i"=>"<a href='$1' target='_blank'>$2</a>",
+				"[:\)]"=>"<span>&#x1F60A;</span>",
+				"[:\(]"=>"<span>&#x1F61F;</span>",
+				"[:\/]"=>"<span>&#x1F615;</span>",
+				"[:D]"=>"<span>&#x1F92D;</span>",
+				"[<3]"=>"<span>&#x2764;</span>",
 				"@\?help\?@i"=>"[link=url do site]titulo do site [/link]<br>[b]<strong>palavra em negrito</strong>[/b]<br>[i]<i>palavra em italica</i>[/i]<br>[s]<strong>palavra sublinada</s>[/s]<br>[quote]cita um comntario de alguém[/quote]<br>[color= o valor da cor]cor da palavra[/color]<br>[youtube=url do video]um titulo por video[/youtube]<br>[img=url da image][/img]<br>"
 		
 		
@@ -35,6 +40,6 @@
 		
 	}
 		
-
+	//echo bbcode ("<3");
 
 ?>
