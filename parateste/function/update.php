1e944247f381b8da3099b0ec1abd586803818efc
root <giovannepm@gmail.com>
2023-04-26 22:21:49 -0300

    correçãoes

--- parateste/function/update.php
+++ parateste/function/update.php
@@ -1,18 +1,40 @@
 <?php
 
 	$gatos=array_diff(scandir("defaultstyle/"),['.','..']);
-	$data=fopen("function/data","r+"); 
-	$datadd=fgets($data); //echo $datadd;
-	fclose ($data);
+	$gatos2=array_diff(scandir("function/"),['.','..']);
+	$mesa=date("m");
+	$dai=date("d");
+	$rora=date("h");
+	$munuta=date("i");
 
 	foreach ($gatos as $gatomm){
 		
 		
 		
-		$ver=str_replace(".css","",$gatomm) . date("m d h:i",filemtime("defaultstyle/". $gatomm));
+		$maluco=date("m",filemtime("defaultstyle/". $gatomm)); //echo $maluco;
+		$maluco2=date("d",filemtime("defaultstyle/". $gatomm));
+		$maluco3=date("h",filemtime("defaultstyle/". $gatomm));
+		$maluco4=date("i",filemtime("defaultstyle/". $gatomm));
 		
-		if (date("m d h:i",filemtime("defaultstyle/". $gatomm))>$datadd){
-		echo $ver;
+		if (($maluco>=$mesa) and ($maluco2>=$dai) and ($maluco3>=$rora) and ($maluco4!=$munuta)){
+		echo $gatomm;
+			
+		}
+		
+		
+	}
+
+	foreach ($gatos2 as $gatomm){
+		
+		
+		
+		$maluco=date("m",filemtime("function/". $gatomm)); //echo $maluco;
+		$maluco2=date("d",filemtime("function/". $gatomm));
+		$maluco3=date("h",filemtime("function/". $gatomm));
+		$maluco4=date("i",filemtime("function/". $gatomm));
+		
+		if (($maluco>=$mesa) and ($maluco2>=$dai) and ($maluco3>=$rora) and ($maluco4!=$munuta)){
+		echo $gatomm;
 			
 		}
 		
